b6a8e4b8ef1853904402ca2245734ed8e7f89d15
nmz1z <118689160+nmz1z@users.noreply.github.com>
2022-11-29 20:52:43 -0300

    added projects db

---
+++ data/projects.js
@@ -0,0 +1,26 @@
+var projectsArray = [
+    {
+        link: "https://nmz1z.github.io/ramen-page/",
+        img: "img/ramen-page.png",
+        title: "Ramen Page",
+        text: "My very first project! HTML and CSS only."
+    },
+    {
+        link: "https://nmz1z.github.io/rock-paper-scissors/",
+        img: "img/rock-paper-scissors.png",
+        title: "Rock-Paper-Scissor",
+        text: "Play Rock-Paper-Scissors against the computer!"
+    },
+    {
+        link: "",
+        img: "",
+        title: "Coming soon",
+        text: "Coming soon"
+    },
+    {
+        link: "",
+        img: "",
+        title: "Coming soon",
+        text: "Coming soon"
+    }
+]
