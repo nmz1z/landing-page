a728ed64c1a1508c1e9379fda5ff538ea68f8212
nmz1z <118689160+nmz1z@users.noreply.github.com>
2022-11-29 20:55:17 -0300

    Projects now injected by js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//// VAR
+// VARIABLES
 // slider
 let sliderContainer = document.getElementById("slider");
 let nextBtn = document.getElementById("next");
@@ -60,7 +60,49 @@ function getQuote(){
     quoteText.textContent = quotesArray[index].quote;
     quoteAuthor.textContent = quotesArray[index].author;
 }
+// inject projects in the slider container
+function getProjects(array){
+    for(let i = 0; i < array.length; i++){
+    //add div
+    let box = document.createElement("div");
+    box.classList.add("project-box");
+    sliderContainer.append(box);
+        //add a
+        let link = document.createElement("a");
+        box.append(link);
+        // link.style.width = "100%";
+        link.href = array[i].link;
+            //add div
+            let imgContainer = document.createElement("div"); 
+            link.append(imgContainer);
+            imgContainer.classList.add("project-img");
+                //add img
+                let image = document.createElement("img");
+                image.src = array[i].img;
+                imgContainer.append(image);
+                image.classList.add("project-img");
+                //add mask
+                let mask = document.createElement("div");
+                imgContainer.append(mask);
+                mask.classList.add("project-mask");
+                //add p
+                let text = document.createElement("p");
+                text.classList.add("project-text");
+                imgContainer.append(text);
+                text.textContent = array[i].text;
+        //add div
+            let title = document.createElement("div");
+            title.classList.add("project-name");
+            box.append(title);
+            //add p
+            let titleText = document.createElement("p");
+            title.append(titleText);
+            titleText.textContent = array[i].title;
+    }
+}
+
 // initialize
+getProjects(projectsArray);
 window.addEventListener('resize', adjustByQuery);
 nextBtn.addEventListener('click', goNext);
 prevBtn.addEventListener('click', goBack);
