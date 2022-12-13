1dcbf687ede40681ad3601b543b27705ae8b8175
nmz1z <118689160+nmz1z@users.noreply.github.com>
2022-12-13 15:22:43 -0300

    reordered projects

--- data/projects.js
+++ data/projects.js
@@ -1,15 +1,9 @@
 var projectsArray = [
     {
-        link: "https://nmz1z.github.io/ramen-page/",
-        img: "img/ramen-page.png",
-        title: "Ramen Page",
-        text: "My very first project! HTML and CSS only."
-    },
-    {
-        link: "https://nmz1z.github.io/rock-paper-scissors/",
-        img: "img/rock-paper-scissors.png",
-        title: "Rock-Paper-Scissors",
-        text: "Play Rock-Paper-Scissors against the computer!"
+        link: "https://nmz1z.github.io/etch-a-sketch/",
+        img: "img/etch-a-sketch.png",
+        title: "Etch-a-Sketch",
+        text: "Draw some awesome pixel art!"
     },
     {
         link: "https://nmz1z.github.io/kana-drills/",
@@ -18,9 +12,15 @@ var projectsArray = [
         text: "Practice Katakana and Hiragana in a fun way!"
     },
     {
-        link: "https://nmz1z.github.io/etch-a-sketch/",
-        img: "img/etch-a-sketch.png",
-        title: "Etch-a-Sketch",
-        text: "Draw some awesome pixel art!"
+        link: "https://nmz1z.github.io/rock-paper-scissors/",
+        img: "img/rock-paper-scissors.png",
+        title: "Rock-Paper-Scissors",
+        text: "Play Rock-Paper-Scissors against the computer!"
+    },
+    {
+        link: "https://nmz1z.github.io/ramen-page/",
+        img: "img/ramen-page.png",
+        title: "Ramen Page",
+        text: "My very first project! HTML and CSS only."
     }
 ]
